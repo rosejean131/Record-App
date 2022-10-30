0945bc6141e51351dabaa9e47ffe7d7a4845b762
roseaban131 <filletchicken387@gmail.com>
2022-10-30 11:26:02 +0800

    not yet done

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('ROOT_URL', 'http://localhost/RecordApp/');
+define('DB_HOST',  'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'recordsapp_db');
+?>
